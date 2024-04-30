f465405ea6a666cdd2e88618a9cfea2a49c32846
Elliott Walker <egwalker1@btinternet.com>
2024-05-01 00:20:29 +0100

    Added HTML logic for product description

--- product/feature/src/main/java/com/example/composeapp/product/feature/pdp/PdpScreen.kt
+++ product/feature/src/main/java/com/example/composeapp/product/feature/pdp/PdpScreen.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -73,9 +77,9 @@ internal fun PdpScreen(
     ) { padding ->
         Box(
             modifier =
-                Modifier
-                    .padding(padding)
-                    .fillMaxSize(),
+            Modifier
+                .padding(padding)
+                .fillMaxSize(),
         ) {
             when (uiState) {
                 is LoadedPdpState -> PdpLoadedContent(uiState)
@@ -108,7 +112,10 @@ private fun BackIconButton(onBackClick: () -> Unit) {
 
 @Composable
 private fun PdpLoadedContent(uiState: LoadedPdpState) {
-    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.verticalScroll(state = rememberScrollState()),
+    ) {
         val listState = rememberLazyListState()
         ProductImageCarousel(
             listState = listState,
@@ -123,7 +130,7 @@ private fun PdpLoadedContent(uiState: LoadedPdpState) {
                 Labels(
                     items = labels,
                     modifier =
-                    Modifier.fillMaxWidth(),
+                        Modifier.fillMaxWidth(),
                 )
             }
 
@@ -153,7 +160,12 @@ private fun PdpLoadedContent(uiState: LoadedPdpState) {
             )
         }
 
-        ProductDescription(uiState.product.description)
+        ProductDescription(
+            description = uiState.product.description,
+            modifier = Modifier.padding(horizontal = 16.dp),
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
     }
 }
 
--- product/feature/src/main/java/com/example/composeapp/product/feature/pdp/components/ProductDescription.kt
+++ product/feature/src/main/java/com/example/composeapp/product/feature/pdp/components/ProductDescription.kt
@@ -2,25 +2,24 @@ package com.example.composeapp.product.feature.pdp.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
 import com.example.composeapp.product.data.product.ProductDetails
-import com.example.composeapp.product.feature.R
-import com.example.composeapp.ui.theming.TextSize
 import com.google.android.material.textview.MaterialTextView
 
 @Composable
-fun ProductDescription(description: String) {
-    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Text(
-            text = stringResource(R.string.about_this_product),
-            fontSize = TextSize.Large,
-        )
+fun ProductDescription(
+    description: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier,
+    ) {
         Html(html = description)
     }
 }
@@ -31,12 +30,12 @@ private fun Html(html: String) {
     Column {
         AndroidView(
             factory = { MaterialTextView(it) },
-            update = { it.text = spannedText }
+            update = { it.text = spannedText },
         )
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun ProductDescriptionPreview() {
     ProductDescription(description = ProductDetails.createMock().description)
